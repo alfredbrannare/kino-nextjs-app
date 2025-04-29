bd4f6e303de607a64a5678d9cf18c6d0c7a1ab1d
Kirill <kirillsavorin@gmail.com>
2025-04-29 23:09:14 +0200

    feat: add controller logic to create movie from IMDb ID

--- backend/src/controllers/movies.controller.js
+++ backend/src/controllers/movies.controller.js
@@ -1,27 +1,67 @@
 import Movie from "../models/model.movies.js";
 
+/**
+ * @desc Get all movies from the database
+ * @route GET /api/movies
+ */
 export const listMovies = async (req, res) => {
   const movies = await Movie.find();
   res.json(movies);
 };
 
-//req.params.id = ':id'
+
+/**
+ * @desc Get a single movie by its ID
+ * @route GET /api/movies/:id
+ * @param - req.params.id
+ */
 export const listSelectedMovie = async (req, res) => {
   const movie = await Movie.findById(req.params.id);
   res.json(movie);
 };
 
+/**
+ * @desc Add a new movie to the database using IMDb ID via OMDb API
+ * @route POST /api/movies
+ * @param  - IMDb movie ID ({"id": "tt1234567"})
+ */
 export const addMovie = async (req, res) => {
-  // Kolar att data som behövs finns i body
+  // Data som finns i body
   console.log("Received request body:", req.body);
-  const { title, description, image } = req.body;
-  if (!title || !description || !image) {
+  if (!req.body || !req.body.id) {
     return res.status(400).json({
-      status: "Film data is required",
+      status: "Imdb film id is requied",
     });
   }
 
-  const movie = new Movie(req.body);
-  await movie.save();
-  res.json(movie);
+  //Hämtar film data från OMDb API
+  try {
+    const response = await fetch(
+      `http://www.omdbapi.com/?i=${req.body.id}&apikey=5cd99ae7`
+    );
+    const data = await response.json();
+
+    const movie = new Movie({
+      title: data.Title,
+      description: data.Plot,
+      year: parseInt(data.Year),
+      image: data.Poster,
+      rating: parseFloat(data.imdbRating),
+    });
+
+    // Kolar om filmen finns redan i databasen
+    const existing = await Movie.findOne({ title: data.Title });
+    if (existing) {
+      return res
+        .status(400)
+        .json({ status: "Movie already exists in the database." });
+    }
+
+    await movie.save();
+    res.json(movie);
+  } catch {
+    res.status(400).json({
+      status: "IMDb ID is invalid or the OMDb API is not responding.",
+    });
+  }
 };
--- backend/src/models/model.movies.js
+++ backend/src/models/model.movies.js
@@ -3,7 +3,9 @@ import mongoose from "mongoose";
 const moviesSchema = new mongoose.Schema({
   title: String,
   description: String,
+  year: Number,
   image: String,
+  rating: Number,
 });
 
 const Movie = mongoose.model('Movie', moviesSchema);
