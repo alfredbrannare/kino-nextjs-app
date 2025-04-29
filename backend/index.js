1bece434c72da0e79f5c63f52037d80fe6db2ce7
Kirill <kirillsavorin@gmail.com>
2025-04-29 23:54:48 +0200

    feat: authorization via Bearer token for testing

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log(req.method, req.url);
+  console.log(req.headers.host, req.method, req.url);
   next();
 });
 
--- backend/src/routes/movies.router.js
+++ backend/src/routes/movies.router.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { listMovies, listSelectedMovie, addMovie } from "../controllers/movies.controller.js";
+import authEx from "../middleware/authEx.js";
 
 //Rutt hanterare
 const router = express.Router();
 
 router.get("/", listMovies);
 router.get("/:id", listSelectedMovie);
-router.post("/", addMovie);
+router.post("/", authEx, addMovie);
 // router.put("/:id", updateMovie);
 // router.delete("/:id", deleteMovie);
 
