3cf0c2d7100f444c3ccd2063e2d95139faf87005
Kirill <kirillsavorin@gmail.com>
2025-04-29 21:05:33 +0200

    feat: set up Express server with MongoDB connection, dotenv config,log HTTP + URl and error handling

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,40 @@
-console.log('test');
+import express from 'express';
+//Behövs för att använda .env
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+// Måste deklareras högst upp i filen (process.env)
+
+dotenv.config();
+
+// Express init
+const app = express();
+
+// Middleware to parse JSON and log HTTP method and URL
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+});
+
+// Behövs för att lägga ut server på Render sen
+const port = process.env.PORT || 5080;
+
+// Ansluta till databasen
+try {
+  await mongoose.connect(process.env.MONGO_URI);
+  console.log('✅ Connected to MongoDB successfully');
+} catch {
+  console.log('❌ Not connected to MongoDB');
+  console.log(`❌ MONGO_URI: ${process.env.MONGO_URI ? ' Active ' : ' Inactive '}`);
+}
+
+
+
+// Starta server
+app.listen(port, () => {
+  console.log(`✅ Server is running on port ${port}`);
+});
+
+// https://emojipedia.org/
