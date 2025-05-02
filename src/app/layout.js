a1320beb6e6911b93f1aee86d069d4bcfd573572
Kirill <kirillsavorin@gmail.com>
2025-05-03 00:10:04 +0200

    feat: favicon and title update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,13 @@
 import Header from "src/components/Header";
 import "./globals.css";
 
+export const metadata = {
+  title: "Kino bio Uppsala",
+  icons: {
+    icon: "/favicon.ico", // путь из public
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
