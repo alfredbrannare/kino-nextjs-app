a480c65b41a6ae1f5f4ff1d20ce17b86c9248567
jennymakki <jjennyeklundd@hotmail.com>
2025-05-05 17:57:47 +0000

    changed a button to Swedish

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -60,7 +60,7 @@ const Login = () => {
 
                             <div className="modal-action">
                                 <button type="submit" className="btn bg-yellow-400 text-black hover:bg-yellow-300">
-                                    Login
+                                    Logga in
                                 </button>
                             </div>
                         </form>
