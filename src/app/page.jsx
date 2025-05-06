183725e9638e69af3805cdacca855565f3970fef
Alfred <alfred-brannarer@hotmail.com>
2025-05-06 15:07:49 +0200

    feat: Implement a fetch and initial map of return value

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,36 @@
+'use client'
+const { useEffect, useState } = require("react");
+
 const Main = () => {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUpcomingMovies = async () => {
+      try {
+        const res = await fetch('/api/screenings/next');
+        const data = await res.json();
+        setMovies(data);
+      } catch (error) {
+        console.error('Error fetching movies:', error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchUpcomingMovies();
+  }, []);
+
   return (
-    <h1>Home page!</h1>
+    <div>
+      <h1>Home page!</h1>
+
+      <div>
+        {movies.map((movie) => (
+          <h1 key={movie.id}>{movie.title}</h1>
+        ))}
+      </div>
+    </div>
+
   )
 }
 
