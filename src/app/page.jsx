1ec4e6653b105f4f2615ef2bf74549ea14cf826c
Alfred <alfred-brannarer@hotmail.com>
2025-05-07 15:14:01 +0200

    feat: Added a visual user friendly error message if movies wont load

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,20 +1,23 @@
 'use client'
 import MovieCard from "src/components/MovieCard";
 import MovieCardSkeleton from "src/components/MovieCardSkeleton";
+import { Info } from "lucide-react";
 const { useEffect, useState } = require("react");
 
 const Main = () => {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchUpcomingMovies = async () => {
       try {
-        const res = await fetch('/api/screenings/next');
+        const res = await fetch('/api/screenings/net');
         const data = await res.json();
         setMovies(data);
       } catch (error) {
         console.error('Error fetching movies:', error);
+        setError('Vi har för tillfället problem med att hämta filmer.');
       } finally {
         setLoading(false);
       }
@@ -26,6 +29,17 @@ const Main = () => {
     <div>
       <div className="justify-center align-center my-6">
         <h1 className="text-4xl font-weight-700 text-center">Visas nu</h1>
+        {error && (
+          <div className="alert alert-warning shadow-lg justify-center align-center mx-auto my-10 max-w-100">
+            <div className="text-center text-black">
+              <Info />
+              <span >Fel</span>
+            </div>
+            <div className="text-sm text-black">
+              {error}
+            </div>
+          </div>
+        )}
         <div className="flex flex-row justify-start xl:justify-center items-center overflow-x-auto space-x-4 px-4 my-6 mx-4">
           {loading ? (
             Array.from({ length: 5 }).map((_, i) => (
