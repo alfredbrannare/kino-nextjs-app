5cae61cae49a3a32941bbcd18a4265a60cf6247b
Patrik <pat_p3@hotmail.com>
2025-05-07 13:13:05 +0200

    Connect seating layout to database and display booked seats

--- src/app/api/bookings/route.js
+++ src/app/api/bookings/route.js
@@ -1,6 +1,23 @@
 import connectDB from "src/lib/mongodb";
 import Booking from "src/models/model.booking.js";
 
+export async function GET(request) {
+    const { searchParams } = new URL(request.url);
+    const movieId = searchParams.get('movieId');
+    const screeningTime = searchParams.get('screeningTime');
+
+    try {
+        await connectDB();
+        const bookings = await Booking.find({ movieId, screeningTime });
+
+        const allSeats = bookings.flatMap((b) => b.seats);
+        return Response.json(allSeats);
+    } catch (err) {
+        console.error('GET /bookings error:', err);
+        return Response.json({ error: 'Något gick fel'}, { status: 500 });
+    }
+}
+
 export async function  POST(request) {
     const body = await request.json();
     const { movieId, screeningTime, seats, userId } = body;
