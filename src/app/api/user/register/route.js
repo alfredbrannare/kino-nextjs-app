fedcaec108961c12ded455a9e3b8c75cbc536bd4
Kirill <kirillsavorin@gmail.com>
2025-05-10 18:54:07 +0200

    refactor: user register email and password format

--- src/app/api/user/register/route.js
+++ src/app/api/user/register/route.js
@@ -2,7 +2,6 @@ import connectDB from "src/lib/mongodb";
 import bcrypt from "bcrypt";
 import User from "src/models/model.users.js";
 import jwt from "jsonwebtoken";
-import { checkAuth } from "src/lib/auth";
 import { NextResponse } from "next/server";
 
 export const POST = async (req) => {
@@ -12,18 +11,40 @@ export const POST = async (req) => {
   const { name, email, password } = body;
 
   if (!name || !email || !password) {
-    return NextResponse.json({ message: "Name, email, and password are required" }, { status: 400 });
+    return NextResponse.json(
+      { message: "Name, email, and password are required" },
+      { status: 400 }
+    );
+  }
+
+  const validateEmail = (email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  if (!validateEmail(email)) {
+    return NextResponse.json(
+      {
+        message: "Invalid email format",
+      },
+      { status: 400 }
+    );
+  }
+
+  if (password.length < 6) {
+    return NextResponse.json(
+      {
+        message: "Password should be longer than 6 characters",
+      },
+      { status: 400 }
+    );
   }
 
   const existingUser = await User.findOne({ email });
   if (existingUser) {
-    return NextResponse.json({ message: "User with this email already exists" }, { status: 409 });
+    return NextResponse.json(
+      { message: "User with this email already exists" },
+      { status: 409 }
+    );
   }
 
   const hashedPassword = await bcrypt.hash(body.password, 10);
-  
-  // const isPasswordValid =
-  //   await bcrypt.compare('test', hashedPassword);
 
   const newUser = await User.create({
     name: body.name,
@@ -35,8 +56,11 @@ export const POST = async (req) => {
     expiresIn: "24h",
   });
 
-  return NextResponse.json({
-    message: "User is registered successfully",
-    user: {id: newUser.id, name: newUser.name, email: newUser.email, token},
-  }, {status: 201})
+  return NextResponse.json(
+    {
+      message: "User is registered successfully",
+      user: { id: newUser.id, name: newUser.name, email: newUser.email, token },
+    },
+    { status: 201 }
+  );
 };
