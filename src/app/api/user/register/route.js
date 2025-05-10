f1786d0eaed40cf5311ba516fa61c25a57b00e39
Kirill <kirillsavorin@gmail.com>
2025-05-10 19:42:44 +0200

    feat/refactor: user registration

--- src/app/api/user/register/route.js
+++ src/app/api/user/register/route.js
@@ -5,62 +5,74 @@ import jwt from "jsonwebtoken";
 import { NextResponse } from "next/server";
 
 export const POST = async (req) => {
-  await connectDB();
+  try {
+    await connectDB();
 
-  const body = await req.json();
-  const { name, email, password } = body;
+    const body = await req.json();
+    const { name, email, password, passwordRepeat } = body;
 
-  if (!name || !email || !password) {
-    return NextResponse.json(
-      { message: "Name, email, and password are required" },
-      { status: 400 }
-    );
-  }
+    if (!name || !email || !password || !passwordRepeat) {
+      return NextResponse.json(
+        { message: "Name, email, and password are required" },
+        { status: 400 }
+      );
+    }
 
-  const validateEmail = (email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-  if (!validateEmail(email)) {
+    const validateEmail = (email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    if (!validateEmail(email)) {
+      return NextResponse.json(
+        {
+          message: "Invalid email format",
+        },
+        { status: 400 }
+      );
+    }
+
+    if (password.length < 6) {
+      return NextResponse.json(
+        {
+          message: "Password should be longer than 6 characters",
+        },
+        { status: 400 }
+      );
+    }
+
+    if (password !== passwordRepeat) {
+      return NextResponse.json(
+        {
+          message: "The passwords are not the same",
+        },
+        { status: 400 }
+      );
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return NextResponse.json(
+        { message: "User with this email already exists" },
+        { status: 409 }
+      );
+    }
+
+    const hashedPassword = await bcrypt.hash(body.password, 10);
+    const newUser = await User.create({
+      name: body.name,
+      email: body.email,
+      hashedPassword: hashedPassword,
+    });
+
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "24h",
+    });
+
     return NextResponse.json(
       {
-        message: "Invalid email format",
+        message: "User register successful",
+        token,
       },
-      { status: 400 }
+      { status: 200 }
     );
+  } catch (error) {
+    return NextResponse.json({ message: `Error: ${error}` }, { status: 500 });
   }
-
-  if (password.length < 6) {
-    return NextResponse.json(
-      {
-        message: "Password should be longer than 6 characters",
-      },
-      { status: 400 }
-    );
-  }
-
-  const existingUser = await User.findOne({ email });
-  if (existingUser) {
-    return NextResponse.json(
-      { message: "User with this email already exists" },
-      { status: 409 }
-    );
-  }
-
-  const hashedPassword = await bcrypt.hash(body.password, 10);
-
-  const newUser = await User.create({
-    name: body.name,
-    email: body.email,
-    hashedPassword: hashedPassword,
-  });
-
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: "24h",
-  });
-
-  return NextResponse.json(
-    {
-      message: "User is registered successfully",
-      user: { id: newUser.id, name: newUser.name, email: newUser.email, token },
-    },
-    { status: 201 }
-  );
 };
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,9 +5,12 @@ import { useAuth } from './user/AuthData';
 
 const Login = () => {
     const { login, loading } = useAuth();
+    const [isLogin, setIsLogin] = useState(true);
     const [open, setOpen] = useState(false);
     const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
     const [password, setPassword] = useState('');
+    const [passwordRepeat, setPasswordRepeat] = useState('');
 
     const handleLogin = async (e) => {
         e.preventDefault();
@@ -32,6 +35,30 @@ const Login = () => {
         }
     };
 
+    const handleRegister = async (e) => {
+        e.preventDefault();
+        const response = await fetch('/api/user/register',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ email, name, password, passwordRepeat })
+            });
+        const data = await response.json();
+        if (data.token) {
+            login(data.token, data.user);
+            setOpen(false);
+            setIsLogin(true)
+            setEmail('');
+            setPassword('');
+            window.location.reload();
+
+        } else {
+            setOpen(false);
+        }
+    };
+
     return (
         <div>
             <button
@@ -40,13 +67,14 @@ const Login = () => {
             >
                 Logga in
             </button>
-
-            {open && (
+            {/* LOGIN */}
+            {open && isLogin && (
                 <div className="modal modal-open">
                     <div className="modal-box relative  bg-[#2B0404] ">
                         <button
                             className="cursor-pointer absolute top-4 right-4 text-white hover:text-gray-300"
-                            onClick={() => setOpen(false)}
+                            onClick={() => { setOpen(false); setIsLogin(true) }}
+
                             aria-label="Close"
                         >
                             <X size={24} />
@@ -57,7 +85,7 @@ const Login = () => {
                         <form onSubmit={handleLogin} className="space-y-4 mt-4">
                             <div>
                                 <label className="label text-gray-300">
-                                    <span className="label-text text-gray-300">E-post eller telefonnummer</span>
+                                    <span className="label-text text-gray-300">E-post</span>
                                 </label>
                                 <input
                                     type="email"
@@ -86,6 +114,79 @@ const Login = () => {
                                     Logga in
                                 </button>
                             </div>
+                            <span onClick={() => setIsLogin(false)} className={'hover:'}>Inte medlem? Registrera dig här!</span>
+                        </form>
+                    </div>
+                </div>
+            )}
+            {/* REGISTER */}
+            {open && !isLogin && (
+                <div className="modal modal-open">
+                    <div className="modal-box relative  bg-[#2B0404] ">
+                        <button
+                            className="cursor-pointer absolute top-4 right-4 text-white hover:text-gray-300"
+                            onClick={() => { setOpen(false); setIsLogin(true) }}
+
+                            aria-label="Close"
+                        >
+                            <X size={24} />
+                        </button>
+
+                        <h3 className="text-lg font-bold text-gray-200">Skapa konto</h3>
+
+                        <form onSubmit={handleRegister} className="space-y-4 mt-4">
+                            <div>
+                                <label className="label text-gray-300">
+                                    <span className="label-text text-gray-300">E-post</span>
+                                </label>
+                                <input
+                                    type="email"
+                                    className="input input-bordered w-full"
+                                    required
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    value={email}
+                                />
+                                <label className="label text-gray-300">
+                                    <span className="label-text text-gray-300">Namn</span>
+                                </label>
+                                <input
+                                    type="text"
+                                    className="input input-bordered w-full"
+                                    required
+                                    onChange={(e) => setName(e.target.value)}
+                                    value={name}
+                                />
+                            </div>
+
+                            <div>
+                                <label className="label text-gray-300">
+                                    <span className="label-text text-gray-300">Lösenord</span>
+                                </label>
+                                <input
+                                    type="password"
+                                    className="input input-bordered w-full"
+                                    required
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    value={password}
+                                />
+                                <label className="label text-gray-300">
+                                    <span className="label-text text-gray-300">Ange lösenord igen</span>
+                                </label>
+                                <input
+                                    type="password"
+                                    className="input input-bordered w-full"
+                                    required
+                                    onChange={(e) => setPasswordRepeat(e.target.value)}
+                                    value={passwordRepeat}
+                                />
+                            </div>
+
+                            <div className="modal-action">
+                                <button type="submit" className={`btn bg-yellow-400 text-black hover:bg-yellow-300 ${loading ? 'loading' : null}`}>
+                                    Registrera
+                                </button>
+                            </div>
+                            <span onClick={() => setIsLogin(true)} className={'hover:'}>Medlem? Logga in här!</span>
                         </form>
                     </div>
                 </div>
