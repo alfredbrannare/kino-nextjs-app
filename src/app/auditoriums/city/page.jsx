92fc0aae7f745a50a22b2b7ba8187b7e7a487567
Patrik <pat_p3@hotmail.com>
2025-05-10 10:37:38 +0200

    Renamed small-hall to city

--- src/app/auditoriums/city/page.jsx
+++ src/app/auditoriums/city/page.jsx
@@ -3,8 +3,10 @@ import SeatSelector from "src/components/SeatSelector";
 
 const SmallHallPage = () => {
     return (
-        <main className="p-6">
-            <h1 className="text-xl font-bold mb-4">Small Hall - Boka platser</h1>
+        <main className="p-6 text-center">
+            <h1 className="text-xl font-bold mb-4">Salong: "Uppsala City"</h1>
+            <h2 className="text-xl font-bold mb-4">Boka platser</h2>
+
             <SeatSelector
                 movieId="123"
                 screeningTime="2025-05-10 18:00"
