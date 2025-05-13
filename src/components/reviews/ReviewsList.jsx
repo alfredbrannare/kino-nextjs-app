7cdf5bfe34cd8c49a174e9c2362525a0b4d6a228
Henrik <Henrikmattsson89@gmail.com>
2025-05-13 15:59:40 +0200

    added a display problem with the pagination buttons on single movie page, to not show unless there is at least one comment

--- src/components/reviews/ReviewsList.jsx
+++ src/components/reviews/ReviewsList.jsx
@@ -1,16 +1,16 @@
-import { useState } from "react"
-import UserReview from "./UserReview"
+import { useState } from 'react';
+import UserReview from './UserReview';
 
-const REVIEWS_PER_PAGE = 5
+const REVIEWS_PER_PAGE = 5;
 
 export default function ReviewsList({ reviews }) {
-	const [currentPage, setCurrentPage] = useState(1)
+	const [currentPage, setCurrentPage] = useState(1);
 
-	const totalPages = Math.ceil(reviews.length / REVIEWS_PER_PAGE)
+	const totalPages = Math.ceil(reviews.length / REVIEWS_PER_PAGE);
 	const paginatedReviews = reviews.slice(
 		(currentPage - 1) * REVIEWS_PER_PAGE,
 		currentPage * REVIEWS_PER_PAGE
-	)
+	);
 
 	return (
 		<div className="space-y-2">
@@ -24,22 +24,26 @@ export default function ReviewsList({ reviews }) {
 
 			{/* Pagination controls */}
 			<div className="flex justify-center mt-4 space-x-2">
-				<button
-					className="btn"
-					disabled={currentPage === 1}
-					onClick={() => setCurrentPage((prev) => prev - 1)}>
-					Previous
-				</button>
-				<span>
-					Page {currentPage} of {totalPages}
-				</span>
-				<button
-					className="btn"
-					disabled={currentPage === totalPages}
-					onClick={() => setCurrentPage((prev) => prev + 1)}>
-					Next
-				</button>
+				{totalPages === 0 ? null : (
+					<>
+						<button
+							className="btn"
+							disabled={currentPage === 1}
+							onClick={() => setCurrentPage((prev) => prev - 1)}>
+							Previous
+						</button>
+						<span>
+							Page {currentPage} of {totalPages}
+						</span>
+						<button
+							className="btn"
+							disabled={currentPage === totalPages}
+							onClick={() => setCurrentPage((prev) => prev + 1)}>
+							Next
+						</button>
+					</>
+				)}
 			</div>
 		</div>
-	)
+	);
 }
