bc59a893038b4735f61c1ddfea44b7209d744ab3
Kirill <kirillsavorin@gmail.com>
2025-05-24 18:51:15 +0200

    feat: gemini AI route

---
+++ src/app/api/user/chat/route.js
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import { GoogleGenAI } from "@google/genai";
+
+export const POST = async (req) => {
+  const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_API_KEY });
+
+  const body = await req.json();
+  const userMessage = body.msg;
+
+  if (!userMessage) {
+    return NextResponse.json(
+      { error: "Missing 'msg' in JSON body" },
+      { status: 400 }
+    );
+  }
+
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash",
+    contents: [
+      { type: "text", text: userMessage }
+    ],
+  });
+
+  return NextResponse.json({ status: response.text }, { status: 200 });
+};
