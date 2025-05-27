bdbd0aae5f16b0eb89d5c6c2572fe586ebd7c798
Kirill <kirillsavorin@gmail.com>
2025-05-27 18:45:51 +0200

    conflict: domains next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
   images: {
     domains: ['res.cloudinary.com'],
+    domains: ['image.tmdb.org'],
     remotePatterns: [
       {
         protocol: 'https',
@@ -12,19 +12,6 @@ const nextConfig = {
       },
     ],
   },
-=======
-	images: {
-		domains: ['image.tmdb.org'],
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 'm.media-amazon.com',
-				port: '',
-				pathname: '/images/**',
-			},
-		],
-	},
->>>>>>> 9d3469cd81c4a9f60c349f003d5b8f94b99eaf05
 };
 
 export default nextConfig;
