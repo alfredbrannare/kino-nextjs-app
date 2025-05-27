1bb53a9e614843e2a48ccf980bbfd44acb826714
Patrik <pat_p3@hotmail.com>
2025-05-27 20:54:44 +0200

    Removed hardcoded userId

--- src/app/auditoriums/city/page.jsx
+++ src/app/auditoriums/city/page.jsx
@@ -3,7 +3,6 @@ import BookingClient from "src/components/booking/BookingClient";
 export default function BookingPage({ searchParams }) {
     const { movieId, screeningTime } = searchParams;
     const auditorium = "city";
-    const userId = "456"; // ersätt med riktig auth senare
 
     if (!movieId || !screeningTime) {
         return <p className="text-center mt-10 text-gray-400">Laddar visning...</p>;
@@ -14,7 +13,6 @@ export default function BookingPage({ searchParams }) {
             movieId={movieId}
             screeningTime={screeningTime}
             auditorium={auditorium}
-            userId={userId}
         />
     );
 }
